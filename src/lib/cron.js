2badab0f65b92bbe49da0e68a4633831466e5713
Henry Nnamani <henrycnnamani4@gmail.com>
2025-05-23 07:01:49 -0700

    cron add

---
+++ src/lib/cron.js
@@ -0,0 +1,16 @@
+import cron from "cron";
+import https from "https";
+
+const job = new cron.CronJob("*/14 * * * *", function () {
+    https
+        .get(process.env.API_URL, (res) => {
+            if(res.statusCode === 200) console.log("GET request sent successfully");
+            else console.log("Error sending GET request");
+        })
+        .on("error", (e) => console.error(`Got error: ${e.message}`));
+});
+
+export default job;
+
+
+
